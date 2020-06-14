2d844641c71e8a67e9a8264dd8cb42640d3e505e
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2020-06-14 16:25:00 -0700

    Remove storyboards

---
+++ scrolly-chart/ChartViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.white
+    }
+}
+
