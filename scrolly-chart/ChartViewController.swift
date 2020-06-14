cffb4010698d423aca4e84a9e409023e2a5bf4d4
Nadia Barbosa <captainbarbosa@users.noreply.github.com>
2020-06-14 16:43:43 -0700

    UICollectionViewController setup

--- scrolly-chart/ChartViewController.swift
+++ scrolly-chart/ChartViewController.swift
@@ -1,11 +1,33 @@
 import UIKit
 
-class ChartViewController: UIViewController {
+class ChartViewController: UICollectionViewController {
+
+    var chartDataSource: [CGFloat] = [1.0, 2.0, 3.0]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.backgroundColor = UIColor.white
+        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "barCell")
+    }
 
-        self.view.backgroundColor = UIColor.white
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        super.init(collectionViewLayout: layout)
+        print("init'd")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return chartDataSource.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "barCell", for: indexPath)
+        cell.backgroundColor = UIColor.green
+        return cell
     }
 }
 
